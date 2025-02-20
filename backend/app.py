a6a206997a04138bfcb9cc8fff53743b9793fa2f
ShernOC <sherlynea1994@hotmail.com>
2025-02-20 20:25:13 +0300

    installed postgresql

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,44 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_migrate import Migrate
 from models import db, TokenBlocklist
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
 CORS(app) 
+
+# #postgreSQL connection
+# DB_USERNAME=os.getenv("DB_USERNAME") #donex
+# DB_PASSWORD=os.getenv("DB_PASSWORD") # 2609
+# DB_NAME=os.getenv("DB_NAME") # donex_db
+# DB_HOST = os.getenv("DB_HOST",) # local host 
+# DB_PORT = os.getenv("DB_PORT") # 5432
+
+# # app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+#POSTGRESQL  CONNECTION / BACKEND 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://donex:2609@localhost:5432/donex_db"
+
+#SQLITE CONNECTION
+# initialize the donex.db table 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///donex.db'
-migrate = Migrate(app, db)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+print("Database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
 
 db.init_app(app)
+migrate = Migrate(app, db)
+
 app.config["JWT_SECRET_KEY"] = "jiyucfvbkaudhudkvfbt" 
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] =  timedelta(hours=1)
 
 jwt = JWTManager(app)
 jwt.init_app(app)
-
 
 # import all functions in views
 from views import *
@@ -28,12 +49,26 @@ app.register_blueprint(donation_bp)
 app.register_blueprint(admin_bp)
 
 
+#SQLITE 
+@app.route('/')
+def index(): 
+    return jsonify ({"Success":"Donex"})
+
+# # postgreSQL 
+# @app.route('/')
+# def index(): 
+#     return render_template("index.html")
+
+
 @jwt.token_in_blocklist_loader
 def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
     jti = jwt_payload["jti"]
     token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
 
     return token is not None
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 # # Mail Credentials 
